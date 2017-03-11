54f61d3c0363e230073571aed8a71bb0ea96732f
wesbilel <bilel.oueslati@esprit.tn>
2017-03-11 22:35:30 +0200

    Hay l Map a Frarét

---
+++ skiworld-Client/src/main/java/esprit/skiworld/skiworld_Client/MapController.java
@@ -0,0 +1,91 @@
+package esprit.skiworld.skiworld_Client;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.lynden.gmapsfx.GoogleMapView;
+import com.lynden.gmapsfx.MapComponentInitializedListener;
+import com.lynden.gmapsfx.javascript.object.GoogleMap;
+import com.lynden.gmapsfx.javascript.object.LatLong;
+import com.lynden.gmapsfx.javascript.object.MapOptions;
+import com.lynden.gmapsfx.javascript.object.MapTypeIdEnum;
+import com.lynden.gmapsfx.service.geocoding.GeocoderStatus;
+import com.lynden.gmapsfx.service.geocoding.GeocodingResult;
+import com.lynden.gmapsfx.service.geocoding.GeocodingService;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+
+public class MapController implements Initializable, MapComponentInitializedListener {
+    MainApp application;
+    @FXML
+    private GoogleMapView mapView;
+    
+    @FXML
+    private TextField addressTextField;
+    
+    private GoogleMap map;
+    
+    private GeocodingService geocodingService;
+
+    private StringProperty address = new SimpleStringProperty();
+
+   @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        mapView.addMapInializedListener(this);
+        address.bind(addressTextField.textProperty());
+    }   
+
+  
+        @Override
+    public void mapInitialized() {
+        geocodingService = new GeocodingService();
+        MapOptions mapOptions = new MapOptions();
+        //coords = latLong.toString();
+        
+        mapOptions.center(new LatLong(36.7625546,8.6879178))
+                .mapType(MapTypeIdEnum.ROADMAP)
+                .overviewMapControl(false)
+                .panControl(false)
+                .rotateControl(false)
+                .scaleControl(false)
+                .streetViewControl(false)
+                .zoomControl(false)
+                .zoom(12);
+
+        map = mapView.createMap(mapOptions);
+        
+    }
+    
+    @FXML
+    public void addressTextFieldAction(ActionEvent event) {
+        geocodingService.geocode(address.get(), (GeocodingResult[] results, GeocoderStatus status) -> {
+            
+            LatLong latLong = null;
+          
+            if( status == GeocoderStatus.ZERO_RESULTS) {
+                Alert alert = new Alert(Alert.AlertType.ERROR, "No matching address found");
+                alert.show();
+                return;
+            } else if( results.length > 1 ) {
+                Alert alert = new Alert(Alert.AlertType.WARNING, "Multiple results found, showing the first one.");
+                alert.show();
+                latLong = new LatLong(results[0].getGeometry().getLocation().getLatitude(), results[0].getGeometry().getLocation().getLongitude());
+            } else {
+                latLong = new LatLong(results[0].getGeometry().getLocation().getLatitude(), results[0].getGeometry().getLocation().getLongitude());
+            }
+            
+            map.setCenter(latLong);
+
+        });
+    }
+    public void setApp(MainApp application) {
+        this.application = application;
+    }
+   
+}
